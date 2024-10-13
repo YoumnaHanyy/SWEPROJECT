7540e13e1b42b2dbb07af49c8d40ddd46d147881
YoumnaHanyy <yomnamussa@gmail.com>
2024-10-14 00:12:12 +0300

    new

--- Pages/user.php
+++ Pages/user.php
@@ -4,14 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Notes Dashboard</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../styles/style.css">
 </head>
 <body>
 
-   
     <div class="dashboard-container">
-
-       
         <div class="sidebar">
             <div class="user-info">
                 <h2>User's Home</h2>
@@ -19,24 +16,20 @@
             <div class="sidebar-buttons">
                 <button class="btn note">Note</button>
                 <div class="twob">
-                <button class="btn task">Task</button>
-                <button class="btn event">Event</button>
-</div>
+                    <button class="btn task">Task</button>
+                    <button class="btn event">Event</button>
+                </div>
             </div>
-            <ul class="sidebar-menu">
-                <li>Tasks</li>
-                <li>Files</li>
-                <li>Calendar</li>
-                <li>Notebooks</li>
-                <li>Tags</li>
-                <li>Shared with Me</li>
-                <li>Trash</li>
-            </ul>
+            <div class="sidebar-menu">
+                <button class="btn">Tasks</button>
+               <button class="btn">Files</button>
+               <button class="btn">Calendar</button>
+                <button class="btn">Notebooks</button>
+                <button class="btn">Tags</button>
+                <button class="btn">Shared with Me</button>
+                <button class="btn">Trash</button>
+</div>
         </div>
-
-       
-       
-
     </div>
 
 </body>
