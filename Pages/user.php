c76a61e0dfeb5a903b4d90ced6288ca18534342f
YoumnaHanyy <yomnamussa@gmail.com>
2024-10-13 15:49:00 +0300

    first commit

---
+++ Pages/user.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Notes Dashboard</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+   
+    <div class="dashboard-container">
+
+       
+        <div class="sidebar">
+            <div class="user-info">
+                <h2>User's Home</h2>
+            </div>
+            <div class="sidebar-buttons">
+                <button class="btn note">Note</button>
+                <div class="twob">
+                <button class="btn task">Task</button>
+                <button class="btn event">Event</button>
+</div>
+            </div>
+            <ul class="sidebar-menu">
+                <li>Tasks</li>
+                <li>Files</li>
+                <li>Calendar</li>
+                <li>Notebooks</li>
+                <li>Tags</li>
+                <li>Shared with Me</li>
+                <li>Trash</li>
+            </ul>
+        </div>
+
+       
+       
+
+    </div>
+
+</body>
+</html>
