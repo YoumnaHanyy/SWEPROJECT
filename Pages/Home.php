1295eb0875ea9b1afa93096781071400fed9cbe9
selvia <selviaibrahimgrace@gmail.com>
2024-10-13 16:53:40 +0300

    first commit

---
+++ Pages/Home.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>DoneDeal</title>
+    <link rel="stylesheet" href="Home.css">
+</head>
+<body>
+    <div class="container">
+        <header class="header">
+            <div class="logo">
+                <img src="DoneDea.png" alt="DoneDeal Logo">
+                <span>DoneDeal</span>
+            </div>
+            <div class="left-buttons">
+        <button class="login-btn">Log in</button>
+        <button class="signup-btn">Sign up</button>
+    </div>
+
+            <nav>
+                <a href="#">Why DoneDeal</a>
+                <a href="#">Explore</a>
+                <a href="#">Plans</a>
+            
+        </header>
+
+        <main class="main-content">
+            <h1>What will you <br> <span class="highlight">achieve</span> today?</h1>
+            <br>
+            <p>Remember everything and tackle any project with your notes, tasks,<br> and schedule all in one place.</p>
+            <br>
+            <button class="cta-btn">Get DoneDeal for free</button>
+            <br>
+            <p><a href="#" class="login-link">Already have an account? Log in</a></p>
+
+            <div class="options">
+                <div class="option">Wiki</div>
+                <div class="option">Planner</div>
+                <div class="option">Docs</div>
+                <div class="option">Class notes</div>
+                <div class="option">Research</div>
+            </div>
+        </main>
+    </div>
+</body>
+</html>
